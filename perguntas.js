7610f83013c373304def666c17bd89bb7882e8c8
Jogador-1 <00001118454467sp@al.educacao.sp.gov.br>
2024-11-05 22:36:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 2 + 2?',
+    '4, Parabéns você não é Analfabeto'
+)
+
+criaCartao(
+    'Mais Matemática',
+    'Quanto é 9.999 x 9.999?',
+    '99.980.001, você é uma vergonha moleke, só virou a carta pra ver a resposta isso se não pegou da calculadora!'
+)
+
+criaCartao(
+    'Matemática, prometo que é a ultima',
+    'Adriano gostaria de comprar um vídeo game, que a vista estava com 30% de desconto e, por isso, diminuía R$ 60,00 do valor da compra. A outra forma de pagamento oferecida pela loja era comprar no cartão em uma única parcela com 12% de desconto. Se Adriano escolheu pagar o vídeo game com o cartão, qual o valor em reais do desconto concedido?',
+    'a) R$ 24,00 b) R$ 18,00 c) R$ 22,00 d) R$ 14,00, quero ver acertar!'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
